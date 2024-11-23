56095ba35216404bbc0b5f84e7be0be4ab77cfcc
Busapalli Nandiswar <nandiswarb93@gmail.com>
2024-11-23 12:43:25 +0530

    initial commit

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-import { store } from "../App";
 
 function Navbar() {
   const [adminName, setAdminName] = useState("");
